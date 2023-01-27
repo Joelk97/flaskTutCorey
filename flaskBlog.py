01de4d3c0ffd6ae8d9495537ba9ed1582d95b32c
Joelk97 <kuehljoel@gmail.com>
2023-01-27 15:15:33 +0100

    just to tut 3

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,8 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1>Hello World</h1>"
+app.config['SECRET_KEY'] = 'e41e7070b8e306efd1bbab0a552633d7' # got via Pathon >>> import secrets >>> secrets.token_hex(16)
 
+post = [
+    { 
+        "title": "Titolo bello",
+        "content": "First post",
+        "author": "corey sdvsd",
+        "date_posted": "27.01.2023" 
+    },
+    {
+        "title": "Titolo bello2",
+        "content": "First post2",
+        "author": "Joel fdn",
+        "date_posted": "27.01.2023" 
+    }
+]
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=post, title="Home - Blog")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
